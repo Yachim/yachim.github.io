16989e093cc4b8e79cf987b535c61c6739c7b92d
Yachim <jachym.kohout@gmail.com>
2023-01-05 06:57:42 +0100

    Add social button component

---
+++ src/components/SocialButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+	title: string;
+	icon: string;
+}>();
+</script>
+
+<template>
+	<a :title="props.title" class="social-button">
+		<font-awesome-icon :icon="props.icon" />
+	</a>
+</template>
