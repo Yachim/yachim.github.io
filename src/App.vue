4d48173222a7fd7303d853ab718d98a64fcb0107
Yachim <jachym.kohout@gmail.com>
2023-01-07 21:57:58 +0100

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import NavBar from "./components/NavBar.vue";
+import FooterComponent from "./components/FooterComponent.vue";
 </script>
 
 <template>
 	<NavBar/>
 	<RouterView />
+	<FooterComponent />
 </template>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,17 @@
+<template>
+<footer class="flex justify-center items-center p-4 gap-4">
+	<p>
+		<a target="_blank" href="https://github.com/yachim/yachim.github.io" class="underline text-accent hover:opacity-[0.65] transition-opacity duration-300">Project</a>
+		by
+		<a target="_top" href="/" class="underline text-accent hover:opacity-[0.65] transition-opacity duration-300">Jáchym Kohout</a>
+	</p>
+	<p class="flex gap-2">
+		<a class="hover:text-accent transition-colors duration-300" href="https://github.com/Yachim" title="GitHub">
+			<font-awesome-icon icon="fa-brands fa-github" />
+		</a>
+		<a class="hover:text-accent transition-colors duration-300" href="https://www.linkedin.com/in/jachym-kohout/" title="LinkedIn">
+			<font-awesome-icon icon="fa-brands fa-linkedin" />
+		</a>
+	</p>
+</footer>
+</template>
