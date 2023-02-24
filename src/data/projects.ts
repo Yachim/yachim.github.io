e7b68317c7cd8ab8c98a83e80f7e0b47f935602b
Yachim <jachym.kohout@gmail.com>
2023-02-24 19:09:02 +0100

    Add description to sfpets

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -10,70 +10,16 @@ const ProjectsData: {
 }[] = [
 		{
 			name: "SfPets",
-			description: "Lorem [ipsum] dolor sit, amet consectetur adipisicing elit. \
-			Possimus rem consequuntur praesentium expedita! \
-			Doloremque molestiae [dolor porro nobis] ipsam vitae voluptates illum et? \
-			Iste explicabo debitis quo dignissimos [velit] eaque?",
+			description: "A helper for the popular online game [Shakes & Fidget]. \
+			Once a user reaches certain level, they gain access to a collectible minigame - [pets]. \
+			Each pet has different [requirements] for it to be collectable. \
+			For example, some pets are available at night, others during the day. \
+			This app [checks the availability] and lets the user [mark collected pets].",
 			image1: "https://i.picsum.photos/id/717/1600/900.jpg?hmac=URP2_XVpUW2zMkDWsw7RSF-3o5qCtUkvtV_w2hM8cBY",
 			image2: "https://i.picsum.photos/id/326/1600/900.jpg?hmac=Jo_9gPdNz5k_eRrpQLL_gEYfEI8p6UEiPzhXn86nUTM",
 			siteUrl: "https://sfpets.yachim.cz",
 			ghUrl: "https://github.com/yachim/sfpets"
-		},
-		{
-			name: "SfPets",
-			description: "Lorem [ipsum] dolor sit, amet consectetur adipisicing elit. \
-			Possimus rem consequuntur praesentium expedita! \
-			Doloremque molestiae [dolor porro nobis] ipsam vitae voluptates illum et? \
-			Iste explicabo debitis quo dignissimos [velit] eaque?",
-			image1: "https://i.picsum.photos/id/717/1600/900.jpg?hmac=URP2_XVpUW2zMkDWsw7RSF-3o5qCtUkvtV_w2hM8cBY",
-			image2: "https://i.picsum.photos/id/326/1600/900.jpg?hmac=Jo_9gPdNz5k_eRrpQLL_gEYfEI8p6UEiPzhXn86nUTM",
-			siteUrl: "https://sfpets.yachim.cz",
-			ghUrl: "https://github.com/yachim/sfpets"
-		},
-		{
-			name: "SfPets",
-			description: "Lorem [ipsum] dolor sit, amet consectetur adipisicing elit. \
-			Possimus rem consequuntur praesentium expedita! \
-			Doloremque molestiae [dolor porro nobis] ipsam vitae voluptates illum et? \
-			Iste explicabo debitis quo dignissimos [velit] eaque?",
-			image1: "https://i.picsum.photos/id/717/1600/900.jpg?hmac=URP2_XVpUW2zMkDWsw7RSF-3o5qCtUkvtV_w2hM8cBY",
-			image2: "https://i.picsum.photos/id/326/1600/900.jpg?hmac=Jo_9gPdNz5k_eRrpQLL_gEYfEI8p6UEiPzhXn86nUTM",
-			siteUrl: "https://sfpets.yachim.cz",
-			ghUrl: "https://github.com/yachim/sfpets"
-		},
-		{
-			name: "SfPets",
-			description: "Lorem [ipsum] dolor sit, amet consectetur adipisicing elit. \
-			Possimus rem consequuntur praesentium expedita! \
-			Doloremque molestiae [dolor porro nobis] ipsam vitae voluptates illum et? \
-			Iste explicabo debitis quo dignissimos [velit] eaque?",
-			image1: "https://i.picsum.photos/id/717/1600/900.jpg?hmac=URP2_XVpUW2zMkDWsw7RSF-3o5qCtUkvtV_w2hM8cBY",
-			image2: "https://i.picsum.photos/id/326/1600/900.jpg?hmac=Jo_9gPdNz5k_eRrpQLL_gEYfEI8p6UEiPzhXn86nUTM",
-			siteUrl: "https://sfpets.yachim.cz",
-			ghUrl: "https://github.com/yachim/sfpets"
-		},
-		{
-			name: "SfPets",
-			description: "Lorem [ipsum] dolor sit, amet consectetur adipisicing elit. \
-			Possimus rem consequuntur praesentium expedita! \
-			Doloremque molestiae [dolor porro nobis] ipsam vitae voluptates illum et? \
-			Iste explicabo debitis quo dignissimos [velit] eaque?",
-			image1: "https://i.picsum.photos/id/717/1600/900.jpg?hmac=URP2_XVpUW2zMkDWsw7RSF-3o5qCtUkvtV_w2hM8cBY",
-			image2: "https://i.picsum.photos/id/326/1600/900.jpg?hmac=Jo_9gPdNz5k_eRrpQLL_gEYfEI8p6UEiPzhXn86nUTM",
-			siteUrl: "https://sfpets.yachim.cz",
-			ghUrl: "https://github.com/yachim/sfpets"
-		},
-		{
-			name: "SfPets",
-			description: "Lorem [ipsum] dolor sit, amet consectetur adipisicing elit. \
-			Possimus rem consequuntur praesentium expedita! \
-			Doloremque molestiae [dolor porro nobis] ipsam vitae voluptates illum et? \
-			Iste explicabo debitis quo dignissimos [velit] eaque?",
-			image1: "https://i.picsum.photos/id/717/1600/900.jpg?hmac=URP2_XVpUW2zMkDWsw7RSF-3o5qCtUkvtV_w2hM8cBY",
-			image2: "https://i.picsum.photos/id/326/1600/900.jpg?hmac=Jo_9gPdNz5k_eRrpQLL_gEYfEI8p6UEiPzhXn86nUTM",
-			siteUrl: "https://sfpets.yachim.cz",
-			ghUrl: "https://github.com/yachim/sfpets"
-		},
+		}
 	];
 
 export default ProjectsData;
